94b99d0f7ad8a831770999cb55262303e2ebc3a7
Nícolas de Souza Moreira <d2022014472@unifei.edu.br>
2025-05-22 18:12:00 -0300

    feat: update LandingPage component; remove HelloWorld references and adjust template

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -7,12 +7,8 @@ const msg = ref('Hello World!');
 </script>
 
 <template>
-<<<<<<< HEAD:src/views/HelloWorld.vue
   <NavBar />
-  <h1>{{ msg }}</h1>
-=======
   <div class="min-h-screen flex flex-col bg-stone-950">
     <h1 class="text-9xl font-bold text-white pb-5">Home Page</h1>
   </div>
->>>>>>> b428f185aadeca4588785a3b661af0aca2c4f305:src/views/LandingPage.vue
 </template>
